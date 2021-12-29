89ff7908914f92dddd5501a585df159a6669371c
Idan Rofeh <idanrofeh@gmail.com>
2021-12-29 11:19:14 +0200

    import components

--- app.jsx
+++ app.jsx
@@ -1,12 +1,27 @@
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
+
+import { Home } from "./pages/home.jsx";
+import { BookApp } from "./pages/book-app.jsx";
+import { KeepApp } from "./pages/keep-app.jsx";
+import { MisterMail } from "./pages/mister-mail.jsx";
+
 export function App() {
   return (
-    <section className="app">
-      <header className="main-header">
-        <h1>Appsus</h1>
-      </header>
-      <main>
-        <Appsus />
-      </main>
-    </section>
+    <Router>
+      <section className="app">
+        <header className="main-header">
+          <h1>Appsus</h1>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={BookApp} />
+            <Route exact path="/" component={KeepApp} />
+            <Route exact path="/" component={MisterMail} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </main>
+      </section>
+    </Router>
   );
 }
