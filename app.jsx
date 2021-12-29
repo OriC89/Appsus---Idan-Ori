6ab4e2fca1d0f51465d471595e7a8c08bd65e42d
Idan Rofeh <idanrofeh@gmail.com>
2021-12-29 10:55:53 +0200

    with html and app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <section className="app">
+      <header className="main-header">
+        <h1>Appsus</h1>
+      </header>
+      <main>
+        <CarApp />
+      </main>
+    </section>
+  );
+}
