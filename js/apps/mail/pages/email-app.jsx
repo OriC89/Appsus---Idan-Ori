9891b6fbaec8bb84918a26d5dfca22558b802f85
Idan Rofeh <idanrofeh@gmail.com>
2021-12-30 18:33:34 +0200

    read/unread-details

--- js/apps/mail/pages/email-app.jsx
+++ js/apps/mail/pages/email-app.jsx
@@ -1,3 +1,5 @@
+const { Route, Switch } = ReactRouterDOM;
+
 import { emailService } from "../services/mail.service.js";
 import { eventBusService } from "../../../services/event-bus-service.js";
 // import { storageService } from "../../../services/storage.service.js";
@@ -15,21 +17,25 @@ export class EmailApp extends React.Component {
     this.loadEmails();
   }
 
-  get ctgSearchParam() {
-    const urlSearchParams = new URLSearchParams(this.props.location.search);
-    return urlSearchParams.get("ctg");
-  }
+  // get ctgSearchParam() {
+  //   const urlSearchParams = new URLSearchParams(this.props.location.search);
+  //   return urlSearchParams.get("ctg");
+  // }
 
-  get carsToDisplay() {
-    const { emails } = this.state;
-    const ctg = this.ctgSearchParam;
-    return emails.filter((email) => !ctg || email.ctg === ctg);
-  }
+  // get carsToDisplay() {
+  //   const { emails } = this.state;
+  //   const ctg = this.ctgSearchParam;
+  //   return emails.filter((email) => !ctg || email.ctg === ctg);
+  // }
 
   loadEmails = () => {
+    emailService
+      .query({ category: "unread", search: "" })
+      .then((unReadEmails) => {
+        eventBusService.emit("unReadEmails-count", unReadEmails.length);
+      });
     const { filterBy } = this.state;
     emailService.query(filterBy).then((emails) => {
-      eventBusService.emit("emails-count", emails.length);
       this.setState({ emails });
     });
   };
@@ -42,10 +48,15 @@ export class EmailApp extends React.Component {
     const { emails } = this.state;
     return (
       <section className="email-app">
-        <React.Fragment>
-          <EmailNav onSetFilter={this.onSetFilter} />
-          <EmailList emails={emails} />
-        </React.Fragment>
+        <EmailNav onSetFilter={this.onSetFilter} />
+        <Switch>
+          <Route exact path="/email-app/:emailId" component={EmailDetails} />
+          <Route
+            exact
+            path="/email-app"
+            render={(props) => <EmailList emails={emails} />}
+          />
+        </Switch>
       </section>
     );
   }
